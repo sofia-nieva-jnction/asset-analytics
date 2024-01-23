3355f44a3134b3a413e1432d026eb8b7d2344dc9
sofia-nieva-jnction <143505363+sofia-nieva-jnction@users.noreply.github.com>
2024-01-23 12:19:24 +0000

    test

--- analysis_app.py
+++ analysis_app.py
@@ -283,6 +283,8 @@ with tab1:
 
                 day1_attributes = radar[(radar.datetime >= str(day1_start)) & (radar.datetime <= str(pd.to_datetime(day1_end) + datetime.timedelta(days=1)))]['attribute'].unique()
                 st.write(day1_attributes)
+                st.write(class_attributes_traces)
+                st.write([x for x in class_attributes_traces if x in day1_attributes])
                 attribute_trace1 = st.selectbox('Select Attribute to plot',
                                                 [x for x in class_attributes_traces if x in day1_attributes],
                                                 key = 'att1')
