12a8a541c7a7328a6b187a72471985919a9a8bbe
sofia-nieva-jnction <143505363+sofia-nieva-jnction@users.noreply.github.com>
2024-02-28 14:27:06 +0000

    Update analysis_app.py

--- analysis_app.py
+++ analysis_app.py
@@ -65,7 +65,7 @@ with tab1:
       
         worst_perfoming_table = get_worst_perfoming_table(route, asset_class)
 
-        c1, s1, c2 = st.columns([0.26, 0.05, 0.2])
+        c1, s1, c2 = st.columns([0.26, 0.15, 0.05])
         with c1:
             asset_number_search_worst = st.text_input('Search Asset', placeholder = 'Input Asset Number', key='search_worst')   
         with s1:
